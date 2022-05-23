1bf6dc9e66a8f357f767e7d667943690f4d28d06
Celirbis <rina-ashino@yandex.ru>
2022-05-23 15:11:13 +0300

    First task
    First task solved correctly, jasmine 3/11

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,15 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = Math.pow(b, 2) - 4 * a * c;
+  if (d < 0) return arr;
+  if (d === 0) arr.push(-b / (2 * a));
+  else {
+    arr.push( (-b + Math.sqrt(d)) / (2 * a) ); 
+    arr.push( (-b - Math.sqrt(d)) / (2 * a) );
+  }
+  return arr; 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
