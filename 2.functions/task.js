5c29c1692134f348bcb0b74237f11b070759e76b
Celirbis <rina-ashino@yandex.ru>
2022-05-26 18:29:29 +0300

    tasks 1, 2, 3 complited

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,57 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0], max = arr[0], sum = arr[0], avg;
 
-  // Ваш код
+  for (let i=1; i < arr.length; i++) {
+    if (arr[i] < min){
+      min = arr[i];
+    }
+    if (arr[i] > max){
+      max = arr[i];
+    }
+    sum = sum + arr[i];
+  }
+  avg = Number((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let i=0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity, workResult;
 
-  // Ваш кода
-  // for ...
-  
+    for (let i=0; i < arrOfArr.length; i++) {
+    workResult = func(arrOfArr[i]);
+    if (workResult > max) {
+      max = workResult;
+    }
+  }
+
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = arr[0], max = arr[0]; 
+  for (let i=1; i < arr.length; i++) {
+    if (arr[i] < min){
+      min = arr[i];
+    }
+    if (arr[i] > max){
+      max = arr[i];
+    }
+  }
+  return Math.abs(max - min);
 }
+  // Ваш код
+
