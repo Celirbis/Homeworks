f70e15badef99740c7dbe23b0662dd6eb368965f
Celirbis <rina-ashino@yandex.ru>
2022-05-31 14:59:33 +0300

    tasks from 4.objects completed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,43 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined) { 
+    this.marks = [mark];
+  } 
+  else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if(this.marks === undefined) { 
+    this.marks = [...marks];
+  } 
+  else for (mark of marks) {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  if(this.marks === undefined) { 
+    this.marks = [...marks];
+  } 
+  else { 
+    return this.marks.reduce((acc, item) => acc = acc + item) / this.marks.length; 
+  }
+}
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
+}
+
