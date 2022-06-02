7a4efe3a7f500b53877dd742e33aaa80cdd5ad10
Celirbis <rina-ashino@yandex.ru>
2022-06-02 21:12:27 +0300

    5.3 complited?

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -94,3 +94,54 @@ class Library {
       return null;
    }
 }
+
+class Student {
+   constructor (name) {
+      this.name = name;
+      this.subjects = [];
+   }
+
+   addMark(mark, subject) {
+      if (mark > 5 || mark < 1) {
+         return "Ошибка, оценка должна быть числом от 1 до 5";
+      }
+      for (const subj of this.subjects) {
+         if (subj.name === subject) {
+            subj.marks.push(mark);
+            return;
+         }
+      }
+      let newSubject = new Subject(subject);
+      newSubject.marks.push(mark);
+      this.subjects.push(newSubject);
+   }
+
+   getAverageBySubject(name) {
+      for (const subject of this.subjects) {
+         if (subject.name === name) {
+            return subject.getAverage();
+         }
+      }
+      return "Несуществующий предмет";
+   }
+
+   getAverage() {
+      return this.subjects.reduce((acc, item) => acc + item.getAverage(), 0) / this.subjects.length;
+   }
+
+   exclude(reason) {
+      this.excluded = reason;
+      delete this.subjects;
+   }
+}
+
+class Subject {
+   constructor (name) {
+      this.name = name;
+      this.marks = [];
+   }
+   getAverage() {
+      return this.marks.reduce((acc, item) => acc + item) / this.marks.length;
+   }
+}
+
