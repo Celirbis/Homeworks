ef7e0b61d0c98ec6b18771136f2161d4951170fb
Celirbis <rina-ashino@yandex.ru>
2022-06-11 18:13:45 +0300

    8.1 completed

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,5 +1,24 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = {length: 0};
+
+  function wrapper(...args) {
+    const hash = args.join(',');
+
+    if (hash in cache) {
+      return ("Из кэша: " + cache[hash]);
+    } else {
+      if (cache.length === 5) {
+      delete cache[Object.keys(cache)[1]];
+      }
+      else {
+        cache.length++;
+      }
+      let result = func(...args);
+      cache[hash] = result;
+      return ("Вычисляем: " + result);
+    }
+  } 
+  return wrapper;
 }
 
 
