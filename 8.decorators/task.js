fb8de180385098b35050524018f2d8e4a5593ccd
Celirbis <rina-ashino@yandex.ru>
2022-06-12 17:15:06 +0300

    8.3 8.4 completed (?)

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -7,12 +7,14 @@ function cachingDecoratorNew(func) {
     if (hash in cache) {
       return ("Из кэша: " + cache[hash]);
     } else {
+
       if (cache.length === 5) {
       delete cache[Object.keys(cache)[1]];
       }
       else {
         cache.length++;
       }
+
       let result = func(...args);
       cache[hash] = result;
       return ("Вычисляем: " + result);
@@ -22,10 +24,39 @@ function cachingDecoratorNew(func) {
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let timeout;
+  let flag = false;
+  
+  return function (...args) {
+      clearTimeout(timeout);
+    if (!flag) {
+      func(...args);
+      flag = true;
+    } 
+    timeout = setTimeout(() => {
+        func.apply(this, args);
+        flag = false;
+      }, ms);
+  };
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let timeout;
+  let flag = false;
+  
+  function wrapper(...args) {
+      wrapper.count++;
+      clearTimeout(timeout);
+    if (!flag) {
+      func(...args);
+      flag = true;
+    } 
+    timeout = setTimeout(() => {
+        func.apply(this, args);
+        flag = false;
+      }, ms);
+  }
+  wrapper.count = 0;
+  return wrapper;
 }
